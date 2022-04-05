8ac246543745afb7c339605ee7effb73bbe229f9
liupengbo <1085636515@qq.com>
2022-04-05 16:27:07 +0800

    gateway

---
+++ base/gateway/src/main/java/com/my/base/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.my.base.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author liu peng bo
+ * @date 2022/4/5 下午3:38
+ */
+@Slf4j
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+        log.info("gateway started!");
+    }
+}
