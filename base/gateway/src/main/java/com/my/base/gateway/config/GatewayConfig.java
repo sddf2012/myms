87fb22839fbd439562fb4bb631d2ecf6abe443ae
liupengbo <1085636515@qq.com>
2022-04-05 17:48:56 +0800

    gateway

---
+++ base/gateway/src/main/java/com/my/base/gateway/config/GatewayConfig.java
@@ -0,0 +1,12 @@
+package com.my.base.gateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liu peng bo
+ * @date 2022/4/5 下午5:47
+ */
+@Configuration
+public class GatewayConfig {
+
+}
