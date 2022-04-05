f2a799fafed8b7745214b6128e7187fb94cc52c4
liu peng bo <1085636515@qq.com>
2022-04-05 18:11:05 +0800

    player

--- business/player/src/main/java/com/my/business/player/controller/PlayerController.java
+++ business/player/src/main/java/com/my/business/player/controller/PlayerController.java
@@ -1,6 +1,6 @@
 package com.my.business.player.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/player/base")
 public class PlayerController {
-    @PostMapping("/getClubPlayer")
-    public List<String> getClubPlayer(@RequestParam String clubCode) {
+    @GetMapping("/getClubPlayer")
+    public List<String> getClubPlayer(@RequestParam(required = false) String clubCode) {
         List<String> players = new ArrayList<>();
         players.add("c luo");
         players.add("messi");
